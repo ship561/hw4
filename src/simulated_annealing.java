18c8093bb5309004fe95d2e6da760a4b32ad9ceb
shermin <wing561@gmail.com>
2011-04-03 22:12:16 -0400

    finalized simulated annealing

--- src/simulated_annealing.java
+++ src/simulated_annealing.java
@@ -5,17 +5,21 @@ import java.util.Random;
 
 
 public class simulated_annealing {
-	int[] A = {1,2,3,5};
-	int[] B = {2,2,3,4};
-	int[] C = {1,1,1,2,2,2,2};
+	//int[] A = {1,2,3,5};
+	//int[] B = {2,2,3,4};
+	//int[] C = {1,1,1,2,2,2,2};
+	int[] A = { 5509,5626,6527,6766,7233,16841};
+	int[] B = { 3526,4878,5643,5804,7421,21230};
+	int[] C = { 1120,1868,2564,2752,3240,3526,3758,3775,4669,5509,15721};
 	int n;
+	
 	simulated_annealing() {
 		for(int i=0; i<A.length; i++) {
 			n+=A[i];
 		}
 	}
 
-	int[] multiset_C(int[] a, int[] b) {
+	int[] multiset_C(int[] a, int[] b) {		//generates a new multiset C from sets A and B
 		int x = 0, y=0, k=0;
 		int Sa=0, Sb=0, Sc=0;
 		LinkedList<Integer> c = new LinkedList<Integer>();
@@ -57,14 +61,15 @@ public class simulated_annealing {
 		return C;
 	}
 	
-	double fitness(int[] cmu) {
+	double fitness(int[] cmu) {		//returns fitness function
 		double sum=0;
 		for(int i=0; i<cmu.length; i++) {
 			sum += Math.pow(cmu[i]-this.C[i], 2)/this.C[i];
 		}
 		return sum;
 	}
-	int[] randomN(int[] arr) {
+	int[] randomN(int[] arr) {		//randomizes the array to produce next states
+		int[] tempArr = arr;
 		Random r = new Random();
 		for (int i=0; i<arr.length; i++) {
 		    int randomPosition = r.nextInt(arr.length);
@@ -72,26 +77,30 @@ public class simulated_annealing {
 		    arr[i] = arr[randomPosition];
 		    arr[randomPosition] = temp;
 		}
-		return arr;
+		return tempArr;
 	}
+	
 	public static void main(String[] args) {
 		simulated_annealing sa = new simulated_annealing();
 		double best=10000;
 		int[] cMS = new int[1];
-		double T = sa.n;
 		int iter=0;
-		while(best >0) {
-			int[] mu = sa.randomN(sa.A);
-			int[] sigma = sa. randomN(sa.B);
-			//int[] mu = sa.A;
-			//int[] sigma = sa.B;
+		while(best >0) {					//continues until the minimum is found. when fitness =0
+			//int[] mu = sa.randomN(sa.A);
+			//int[] sigma = sa. randomN(sa.B);
+			int[] mu = sa.A;
+			int[] sigma = sa.B;
 			cMS=sa.multiset_C(mu,sigma);
 			Arrays.sort(cMS);
 			double fit = sa.fitness(cMS);
-			if(best > fit) {
+			if(best > fit) {				//stores best fitness score
+				sa.A=mu;
+				sa.B=sigma;
 				best = fit;
+			} else {						//moves to next state randomly
+				sa.A = sa.randomN(sa.A);
+				sa.B = sa. randomN(sa.B);
 			}
-			T=T-1/T;
 			iter++;
 		}
 		System.out.print("iterations: " + iter + " C: ");
@@ -99,11 +108,18 @@ public class simulated_annealing {
 			System.out.print(cMS[i] + " ");
 		}
 		System.out.println("");
-		for(int i=0; i<sa.A.length; i++) 
-			System.out.print(sa.A[i] + " ");
+		int temp=0;
+		for(int i=0; i<sa.A.length; i++){ 
+			temp += sa.A[i];
+			System.out.print(temp + " ");
+		}
 		System.out.println("");
-		for(int i=0; i<sa.B.length; i++)
-			System.out.print(sa.B[i] + " ");
+		temp=0;
+		for(int i=0; i<sa.B.length; i++){
+			temp += sa.B[i];
+			System.out.print(temp + " ");
+		}
+			
 	}
 
 }
